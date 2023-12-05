1271427be8200b0d64604ca82b084ab1257c88e4
XihuaYang <xihua.yang@fashionnova.com>
2023-12-05 14:03:08 -0800

    add git tag helper

---
+++ scripts/delete_tag.sh
@@ -0,0 +1,7 @@
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    echo "Missing version"
+    exit 1
+fi
+git tag -d $VERSION
+git push origin --delete $VERSION
---
+++ scripts/tag_and_push.sh
@@ -0,0 +1,8 @@
+VERSION=$1
+MESSAGE=$2
+if [ -z "$VERSION" ] || [ -z "$MESSAGE" ]; then
+    echo "Missing version or message"
+    exit 1
+fi
+git tag -a $VERSION -m "$MESSAGE"
+git push origin $VERSION
