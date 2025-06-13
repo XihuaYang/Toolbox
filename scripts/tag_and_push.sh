5f5c796521a0b75be94331f98ea6a266f32b79bd
Xihua Yang <xihua.yang@fashionnova.com>
2025-06-13 15:54:15 -0700

    Allow lightweight tag

--- scripts/tag_and_push.sh
+++ scripts/tag_and_push.sh
@@ -2,9 +2,13 @@
 
 VERSION=$1
 MESSAGE=$2
-if [ -z "$VERSION" ] || [ -z "$MESSAGE" ]; then
-    echo "Missing version or message"
+if [ -z "$VERSION" ]; then
+    echo "Missing version"
     exit 1
 fi
-git tag -a "$VERSION" -m "$MESSAGE"
+if [ -z "$MESSAGE" ]; then
+    git tag "$VERSION"
+else
+    git tag -a "$VERSION" -m "$MESSAGE"
+fi
 git push origin "$VERSION"
